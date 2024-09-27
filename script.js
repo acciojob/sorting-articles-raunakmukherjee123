7247b41825addc6476cee11a77139bc6b377a39f
Raunak Mukherjee <153613701+raunakmukherjee123@users.noreply.github.com>
2024-09-27 22:02:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function remove(bandname) {
+  return bandname.replace(/^(a |an |the )/i, '').trim();
+}
+
+const sortedBands = bands.sort((a, b) => remove(a).localeCompare(remove(b)));
+
+const ulist = document.getElementById("band");
+
+sortedBands.forEach(band => {
+  const listItem = document.createElement("li");
+  listItem.textContent = band;
+  ulist.appendChild(listItem);
+});
